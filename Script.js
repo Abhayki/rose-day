761676309c65a8ef2d57fa980c29d09de4b60bc8
Abhayki <163877200+Abhayki@users.noreply.github.com>
2025-02-06 20:30:50 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    let message = document.getElementById("message");
+    message.innerHTML = "Wishing you a day full of love and happiness! ❤️🌹";
+    message.style.opacity = "1";
+}
